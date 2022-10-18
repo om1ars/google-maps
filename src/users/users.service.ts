4c0deded6ebfd51daec82a056b8f90fb58a4ee53
omar <forfirebasejs2@gmail.com>
2022-10-18 15:18:10 -0500

    first commit

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,37 +1,37 @@
+import {Component} from '@nestjs/common';
 
-import { Component } from '@nestjs/common';
 const people = require('./users');
 
 @Component()
-export class UsersService {    
-    
-    getAllUsers(){        
-        return people.map( (person, index) => ({
+export class UsersService {
+
+    getAllUsers() {
+        return people.map((person, index) => ({
             name: person.name,
             position: person.position,
         }));
     }
-   
+
     intializePusher() {
         const Pusher = require('pusher');
         const pusher = new Pusher({
-            appId: 'YOUR_APP_ID',
-            key: 'YOUR_APP_KEY',
-            secret: 'YOUR_SECRET_KEY',
-            cluster: 'CLUSTER',
+            app_id: "1493926",
+            key: "c1db973c8d270c54f43f",
+            secret: "2be83b2eae2f4dcf3f10",
+            cluster: "us2",
             encrypted: true
         });
-        
+
         return pusher;
     }
-    
+
     postLocation(user) {
         const Pusher = require('pusher');
         const {lat, lng} = user.position
-        
-        people.forEach( (person, index) => {
+
+        people.forEach((person, index) => {
             if (person.position.lat === user.position.lat) {
-                people[index] = { ...person, position: { lat, lng } };
+                people[index] = {...person, position: {lat, lng}};
                 return this.intializePusher().trigger('map-geofencing', 'location', {person: people[index], people})
             }
         })
